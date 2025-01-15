cdf2908a1acb0fedc34a4bb93b116f48b627f9f6
BALASURIYA Nadun <nadun.balasuriya@education.wa.edu.au>
2025-01-15 14:49:43 +0800

    Add restrictions for user types

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,33 @@
+// Check the user's role on every page load
+window.onload = function () {
+  const userRole = sessionStorage.getItem("role");
+  const currentPage = window.location.pathname.split("/").pop();
+
+  if (currentPage === "principal_d.html" && userRole !== "Principal") {
+    window.location.href = "index.html";
+  } else if (
+    currentPage === "teacher_d.html" &&
+    userRole !== "Teacher" &&
+    userRole !== "Principal"
+  ) {
+    window.location.href = "index.html";
+  } else if (
+    currentPage === "student_d.html" &&
+    userRole !== "Student" &&
+    userRole !== "Parent" &&
+    userRole !== "Principal"
+  ) {
+    window.location.href = "index.html";
+  } else if (
+    currentPage === "parent_d.html" &&
+    userRole !== "Parent" &&
+    userRole !== "Principal"
+  ) {
+    window.location.href = "index.html";
+  }
+};
+
 function selectRole(role) {
-  // Store the selected role in sessionStorage
   sessionStorage.setItem("role", role);
 
   if (role === "Principal") {
@@ -12,3 +40,5 @@ function selectRole(role) {
     window.location.href = "parent_d.html";
   }
 }
+
+console.log(userRole);
